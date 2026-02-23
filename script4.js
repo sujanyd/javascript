592186adad0b2a6537a39c4cd2f32e888cdd1803
sujanyd <ydsujan@gmail.com>
2026-02-23 10:39:50 +0530

    command message in the files

--- script4.js
+++ script4.js
@@ -1,10 +1,10 @@
-function showCard() {
-    // Get input values
-    const name = document.getElementById('nameInput').value;
-    const age = document.getElementById('ageInput').value;
-    const phone = document.getElementById('phoneInput').value;
+// function showCard() {
+//     // Get input values
+//     const name = document.getElementById('nameInput').value;
+//     const age = document.getElementById('ageInput').value;
+//     const phone = document.getElementById('phoneInput').value;
 
-    // Get elements to display on the back
-    const displayName = document.getElementById('displayName');
-    const displayAge = document.getElementById('displayAge');
-    const displayPhone
+//     // Get elements to display on the back
+//     const displayName = document.getElementById('displayName');
+//     const displayAge = document.getElementById('displayAge');
+//     const displayPhone
